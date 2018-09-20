2b77c703f0eb4e1c52c9f732f8c9cdacdf225c44
Rob Waight <43173714+rwaight@users.noreply.github.com>
2018-09-20 15:36:18 -0500

    Update auto_conf_vm.sh
    Checking for applications before attempting to install them.  Added comments to the end of each set of if statements

--- auto_conf_vm.sh
+++ auto_conf_vm.sh
@@ -25,19 +25,24 @@ then
 elif [[ $MAJOR_VERSION -ge 7 && $MAJOR_VERSION -lt 8 ]]
 then
   echo "Do version 7 things."
-  # Install Java and rpm, assuming they are not installed (CentOS 7 minimal)
+  # Update yum, this is only suggested for lab systems!!
   yum -y update # This is for the lab system only, do not use in production!
-  yum -y install java
-  yum -y install rpm
+  if ! which java; then
+    # Install Java
+    yum -y install java
+  fi # end of: if ! which java
   
   # Install open-vm-tools on a Virtual Machine
   if dmidecode | grep -i vmware; then
-    echo "This is a VMware Virtual Machine"
-    yum -y install open-vm-tools
-    echo "open-vm-tools have been installed"
-    systemctl enable vmtoolsd.service
-    systemctl start vmtoolsd
-  fi
+    echo "This is a VMware Virtual Machine, checking for open-vm-tools"
+    if ! which open-vm-tools; then
+      echo "installing open-vm-tools"
+      yum -y install open-vm-tools
+      systemctl enable vmtoolsd.service
+      systemctl start vmtoolsd
+      echo "open-vm-tools has been installed and started"
+    fi # end of: if ! which open-vm-tools
+  fi # end of: if dmidecode | grep -i vmware
   
   # Prep Elasticsearch install
   rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
@@ -47,6 +52,7 @@ then
   if [ -e $elr ]; then
     echo "File $elr already exists"
   else
+    echo "Creating file $elr and populating it with 6.x info" #Provide feedback
     echo "[elasticsearch-6.x]" >> $elr
     echo "name=Elasticsearch repository for 6.x packages" >> $elr 
     echo "baseurl=https://artifacts.elastic.co/packages/6.x/yum" >> $elr
@@ -56,36 +62,42 @@ then
     echo "autorefresh=1" >> $elr
     echo "type=rpm-md" >> $elr
     echo "Populated $elr with data" #Provide feedback
-  fi  
+  fi # end of: if [ -e $elr ]
   
-  # Install Elasticsearch and make a copy of the original config file
-  yum -y install elasticsearch
-  cd /etc/elasticsearch/
-  cp elasticsearch.yml elasticsearch.yml.backup
-  echo "The elasticsearch.yml file needs to be updated here"
+  if ! which elasticsearch; then
+    # Install Elasticsearch and make a copy of the original config file
+    yum -y install elasticsearch
+    cd /etc/elasticsearch/
+    cp elasticsearch.yml elasticsearch.yml.backup
+    echo "The elasticsearch.yml file needs to be updated here"
+  fi # end of: if ! which elasticsearch
   
-  # Install Kibana and make a copy of the original config file
-  yum -y install kibana
-  cd /etc/kibana/
-  cp kibana.yml kibana.yml.backup
-  echo "The kibana.yml file needs to be updated here"
+  if ! which kibana; then
+    # Install Kibana and make a copy of the original config file
+    yum -y install kibana
+    cd /etc/kibana/
+    cp kibana.yml kibana.yml.backup
+    echo "The kibana.yml file needs to be updated here"
+  fi # end of: if ! which kibana
   
-  # Install Logstash and make a copy of the original config file
-  yum -y install logstash
-  cd /etc/logstash/
-  cp logstash.yml logstash.yml.backup
-  echo "The logstash.yml file needs to be updated here"
+  if ! which logstash; then
+    # Install Logstash and make a copy of the original config file
+    yum -y install logstash
+    cd /etc/logstash/
+    cp logstash.yml logstash.yml.backup
+    echo "The logstash.yml file needs to be updated here"
+  fi # end of: if ! which logstash
   
   # Determine if this is CentOS 7
   CentMajor=$(cat /etc/centos-release | tr -dc '0-9.'|cut -d \. -f1)
 else
   echo "Unhandled version $MAJOR_VERSION."
-fi
+fi # end of: if [[ $MAJOR_VERSION -ge 6 && $MAJOR_VERSION -lt 7 ]]
+
 
 # CentOS 7 minimal has the firewall enabled, we need to open default ports
 # This section is separate from the RedHat major version for this reason.
-if [[ $CentMajor -ge 7 && $CentMajor -lt 8 ]]
-then
+if [[ $CentMajor -ge 7 && $CentMajor -lt 8 ]]; then
   # This needs to be updated to account for the version listed above.
   # This also needs to be moved into the section to perform version 7 actions
   echo "Performing automated system configuration for CentOS 7, assuming CentOS 7 (minimal)"
@@ -96,4 +108,4 @@ then
   firewall-cmd --zone=public --add-port=5601/tcp --permanent
   firewall-cmd --zone=public --add-port=5601/udp --permanent
   echo "Default ports allowed for Elasticsearch and Kibana"
-fi
+fi # end of: if [[ $CentMajor -ge 7 && $CentMajor -lt 8 ]]
